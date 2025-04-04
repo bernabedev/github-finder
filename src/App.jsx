eae36cbe7259436c5e2956898f6e6b58b049fd40
Freilyn Bernabe <bernabe@wifi.local>
2025-04-04 14:33:16 -0400

    add to favorites

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,37 @@ import UserCard from "./components/user-card";
 import { useState } from "react";
 function App() {
   const [user, setUser] = useState(undefined);
+  const [favorites, setFavorites] = useState(new Map());
+
+  const handleAddFavorite = (user) => {
+    const newFavorites = new Map(favorites);
+    newFavorites.set(user.id, user);
+    setFavorites(newFavorites);
+  };
+
   return (
     <>
       <div className="container">
         <h1>Buscador de Github</h1>
         <Search onGetUser={setUser} />
-        {user && <UserCard user={user} />}
+        {user && (
+          <UserCard
+            user={user}
+            onAddFavorite={(user) => handleAddFavorite(user)}
+          />
+        )}
+        <h1>Favoritos</h1>
+        {favorites.size > 0 && (
+          <div>
+            {Array.from(favorites.values()).map((user) => (
+              <UserCard
+                key={user.id}
+                user={user}
+                onAddFavorite={(user) => handleAddFavorite(user)}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -1,10 +1,12 @@
-export default function UserCard({ user }) {
+export default function UserCard({ user, onAddFavorite }) {
   return (
     <div className="user-card">
       <img src={user.avatar_url} alt={user.name} />
       <h4>{user.name}</h4>
       <p>{user.location}</p>
       <p className="bio">{user.bio}</p>
+
+      <button onClick={() => onAddFavorite(user)}>Agregar a favoritos</button>
     </div>
   );
 }
